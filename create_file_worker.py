d5c9c01f3569aaeddbb45f99964c48a60be32e84
Chinmaya Kumar Sahoo <ChSahoo@ESTUATE.COM>
2024-01-03 07:34:16 +0530

    Added exception while writting the files

--- create_file_worker.py
+++ create_file_worker.py
@@ -2,13 +2,16 @@ import random
 from multiprocessing.pool import ThreadPool
 
 def create_file(file_name):
-    with open(file_name, "w") as file:
-        file.write(f"This is demo file {file_name}.")
+    try:
+        with open(file_name, "w") as file:
+            file.write(f"This is demo file {file_name}.")
+    except Exception as e:
+        print(f"Failed to create the file with name {file_name} due to {e}")
 
 random_list = []
-for i in range(5):
+for _ in range(5):
     rand_int = random.randint(0,100)
-    file_name = "filename_"+str(rand_int)+".txt"
+    file_name = f"filename_{rand_int}.txt"
     random_list.append(file_name)   
 
 with ThreadPool(5) as pool:
